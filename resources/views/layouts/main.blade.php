a6f0b40fb3d61bcaf2cf47ec97b43372c21899a2
Pavel Sotnikov <inkognitoo92@yandex.ru>
2015-11-07 01:11:51 +0500

    add: yandex метрика

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,9 @@
     <title>@yield('title')</title>
 </head>
 <body>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter32344465 = new Ya.Metrika({ id:32344465, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="https://mc.yandex.ru/watch/32344465" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 <div class="container">
     @yield('content')
 </div>
@@ -18,6 +21,4 @@
     </p>
 </div>
 </body>
-<script src="/js/reqwest.min.js"></script>
-<script src="/js/main.js"></script>
 </html>
