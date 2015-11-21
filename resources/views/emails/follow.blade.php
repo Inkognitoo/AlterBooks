c214704c5f64f4b9d58cd74ea01269d3d2c9772d
Pavel Sotnikov <inkognitoo92@yandex.ru>
2015-11-21 23:03:57 +0500

    update: новый, адаптивный шаблон для письма

--- resources/views/emails/follow.blade.php
+++ resources/views/emails/follow.blade.php
@@ -1,13 +1,57 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
+    <link href='https://fonts.googleapis.com/css?family=Kurale&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
     <title>AlterBooks</title>
 </head>
-<body>
-    <p>«Добрейшего времени суток!  Вы получили данное письмо, потому что подписались на новостную рассылку проектка AlterBooks. Что дальше?<br>
-        Во-первых, вы будете получать новости о разработке проекта. Вам же интересно, правда?<br>
-        Во вторых, вы будете одним из первых людей <span style="text-decoration: line-through;">ступивших на поверхность Марса</span> получивших доступ к рабочей бета-версии проекта, естественно, это доступно будет только <span style="text-decoration: line-through;">кругу избранных</span> людям, подписавшимся на рассылку.</p>
-    <i>С огромным уважением, команда <span style="color: red">A</span>lter<span style="color: red">B</span>ooks.</i>»
-    <p style="font-size: small">Если же вы не поняли ничего из вышесказанного и никакого проекта AlterBooks не знаете, ни на что не подписывались, и получать ничего не желаете - пройдите по <a href="http://alterbooks.ru/unsubscribe?email={{$email}}&code={{$unsubscribe_code}}">этой ссылке</a></p>
+<body color="#ccc">
+<table style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;display:block;max-width:100%;margin:0;padding:20px"><tbody style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;display:block;max-width:100%;margin:0;padding:0"><tr style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;display:block;max-width:100%;margin:0;padding:0"><td style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;margin:0;padding:0"></td>
+        <td bgcolor="#FFFFFF" style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;display:block!important;max-width:600px!important;clear:both!important;margin:0 auto;padding:30px;border:1px solid #f0f0f0">
+
+            <div style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;max-width:600px;display:block;margin:0 auto;padding:0">
+                <table>
+                    <tbody><tr>
+                        <td style="font-size:11px;color:#999999">Книги для людей, а не издательств</td>
+                    </tr>
+                    </tbody></table>
+                <table style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;display:block;max-width:100%;margin:0;padding:0"><tbody style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;display:block;max-width:100%;margin:0;padding:0"><tr style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;display:block;max-width:100%;margin:0;padding:0"><td style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;display:block;max-width:100%;margin:0;padding:0">
+                            <table style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;width:100%;margin:0 0 24px;padding:0"><tbody><tr style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;margin:0;padding:0"><td style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;margin:0;padding:0">
+                                    </td>
+                                </tr></tbody></table>
+                            <p style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:14px;line-height:1.6;font-weight:normal;margin:0 0 24px;padding:0">
+                            </p><div><p>Итак...</p>
+
+                                <p>«Добрейшего времени суток!</p>
+                                <p>Вы получили данное письмо, потому что подписались на новостную рассылку проектка AlterBooks.</p>
+                                <p>Что дальше?</p>
+                                <hr>
+                                <p>1) Во-первых, вы будете получать новости о разработке проекта. Вам же интересно, правда?</p>
+                                <p>2) Во вторых, вы будете одним из первых людей <span style="text-decoration: line-through;">ступивших на поверхность Марса</span> получивших доступ к рабочей бета-версии проекта, естественно, это доступно будет только <span style="text-decoration: line-through;">кругу избранных</span> людям, подписавшимся на рассылку.</p>
+                                <i>С огромным уважением, команда <span style="color: red">A</span>lter<span style="color: red">B</span>ooks.</i>»
+                            </div>
+                            <p></p>
+
+
+
+                        </td>
+                    </tr></tbody></table></div>
+
+        </td>
+        <td style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;margin:0;padding:0"></td>
+    </tr></tbody></table>
+<table style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;width:100%;clear:both!important;margin:0 0 24px;padding:0"><tbody><tr style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;margin:0;padding:0"><td style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;margin:0;padding:0"></td>
+        <td style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;display:block!important;max-width:600px!important;clear:both!important;margin:0 auto;padding:0">
+
+            <div style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;max-width:600px;display:block;margin:0 auto;padding:0">
+                <table style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;display:block;max-width:100%;margin:0;padding:0"><tbody style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;display:block;max-width:100%;margin:0;padding:0"><tr style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;display:block;max-width:100%;margin:0;padding:0"><td align="center" style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;display:block;max-width:100%;margin:0;padding:0">
+                            <p style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:12px;line-height:1.6;color:#666;font-weight:normal;margin:0;padding:0">Если же вы не поняли ничего из вышесказанного и никакого проекта <a href="" style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;color:#999;margin:0;padding:0" target="_blank">AlterBooks</a> не знаете, ни на что не подписывались, и получать ничего не желаете - пройдите по cсылке - <a href="http://alterbooks.ru/unsubscribe?email={{$email}}&code={{$unsubscribe_code}}" style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;color:#999;margin:0;padding:0" target="_blank">Отписаться</a>.</p>
+                            <p style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:12px;line-height:1.6;color:#666;font-weight:normal;margin:0;padding:0">Alterbooks © 2015</p>
+                        </td>
+                    </tr></tbody></table></div>
+
+        </td>
+        <td style="font-family:'Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:100%;line-height:1.6;margin:0;padding:0"></td>
+    </tr></tbody></table>
 </body>
 </html>
