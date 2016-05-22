cb1b2efc7e4b8cbbce839919f8b9ec5d17577db1
Pavel Sotnikov <inkognitoo92@yandex.ru>
2016-05-22 16:41:05 +0500

    update: украшательства для вывода

--- deployment/develop.sh
+++ deployment/develop.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+YELLOW='\033[0;33m'
+RESET='\033[0m'
+
 echo "__________ start deployment __________"
 echo "накатываю миграции"
 php artisan migrate:install
@@ -6,6 +9,11 @@ php artisan migrate
 
 echo "накатываю сиды"
 php artisan db:seed --class=RarsTableSeeder
+echo -e "${YELLOW}Seeding: ${RESET}RarsTableSeeder"
 php artisan db:seed --class=UserStatusesTableSeeder
+echo -e "${YELLOW}Seeding: ${RESET}UserStatusesTableSeeder"
 php artisan db:seed --class=LanguageTableSeeder
+echo -e "${YELLOW}Seeding: ${RESET}LanguageTableSeeder"
+php artisan db:seed --class=BookStatusesTableSeeder
+echo -e "${YELLOW}Seeding: ${RESET}BookStatusesTableSeeder"
 echo "___________ end deployment ____________"
