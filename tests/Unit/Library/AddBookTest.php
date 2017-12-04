b7a66084e8477b9d8cba877c43ec7d1e586681ee
Pavel Sotnikov <inkognitoo92@yandex.ru>
2017-12-04 11:10:11 -0800

    Расширить варианты тестирования библиотеки
    Проверять в том числе, что данные добавились/удалились из бд. Обрабатывать
    авторизацию в новых реалиях, дополнительно тестировать невозможность
    пользователя манипулировать в библиотеке собственными книгами.

--- tests/Unit/Library/AddBookTest.php
+++ tests/Unit/Library/AddBookTest.php
@@ -25,18 +25,24 @@ class AddBookTest extends TestCase
     {
         $this->seed(LibraryTestSeeder::class);
 
+        /** @var User $person */
         $person = factory(User::class)->create();
-        $this->be($person);
-        $book_id = Book::inRandomOrder()
+        $book = Book::inRandomOrder()
             ->first()
-            ->id
         ;
 
-        $response = $this->post(route('api.library.add', ['id' => $book_id]));
+        $headers = [
+            'Authorization' => 'Bearer ' . $person->api_token
+        ];
 
+        $response = $this->post(route('api.library.add', ['id' => $book->id]), [], $headers);
         $response->assertJson([
             'success' => true,
             'code' => 200
+        ]);
+        $this->assertDatabaseHas('users_library', [
+            'user_id' => $person->id,
+            'book_id' => $book->id,
         ]);
     }
 
@@ -49,23 +55,34 @@ class AddBookTest extends TestCase
     {
         $this->seed(LibraryTestSeeder::class);
 
+        /** @var User $person */
         $person = factory(User::class)->create();
-        $this->be($person);
-        $book_id = Book::inRandomOrder()
+        $book = Book::inRandomOrder()
             ->first()
-            ->id
         ;
 
-        $response = $this->post(route('api.library.add', ['id' => $book_id]));
+        $headers = [
+            'Authorization' => 'Bearer ' . $person->api_token
+        ];
+
+        $response = $this->post(route('api.library.add', ['id' => $book->id]), [], $headers);
         $response->assertJson([
             'success' => true,
             'code' => 200
         ]);
+        $this->assertDatabaseHas('users_library', [
+            'user_id' => $person->id,
+            'book_id' => $book->id,
+        ]);
 
-        $response = $this->post(route('api.library.add', ['id' => $book_id]));
+        $response = $this->post(route('api.library.add', ['id' => $book->id]), [], $headers);
         $response->assertJson([
             'success' => false,
             'code' => 400
+        ]);
+        $this->assertDatabaseHas('users_library', [
+            'user_id' => $person->id,
+            'book_id' => $book->id,
         ]);
     }
 
@@ -78,25 +95,70 @@ class AddBookTest extends TestCase
     {
         $this->seed(LibraryTestSeeder::class);
 
+        /** @var User $person */
         $person = factory(User::class)->create();
-        $this->be($person);
         $books = Book::inRandomOrder()
             ->limit(10)
             ->get()
         ;
 
+        $headers = [
+            'Authorization' => 'Bearer ' . $person->api_token
+        ];
+
         foreach ($books as $book) {
-            $response = $this->post(route('api.library.add', ['id' => $book->id]));
+            $response = $this->post(route('api.library.add', ['id' => $book->id]), [], $headers);
             $response->assertJson([
                 'success' => true,
                 'code' => 200
             ]);
+            $this->assertDatabaseHas('users_library', [
+                'user_id' => $person->id,
+                'book_id' => $book->id,
+            ]);
 
-            $response = $this->post(route('api.library.add', ['id' => $book->id]));
+            $response = $this->post(route('api.library.add', ['id' => $book->id]), [], $headers);
             $response->assertJson([
                 'success' => false,
                 'code' => 400
             ]);
+            $this->assertDatabaseHas('users_library', [
+                'user_id' => $person->id,
+                'book_id' => $book->id,
+            ]);
         }
+    }
+
+    /**
+     * Проверяем невозможность добавления в бибилиотеку собственной книги
+     *
+     * @return void
+     */
+    public function testFailAddOwnBookToLibrary()
+    {
+        $this->seed(LibraryTestSeeder::class);
+
+        /** @var User $person */
+        $person = factory(User::class, 10)->create()->each(function ($u) {
+            /** @var User $u */
+            $u->books()->save(factory(Book::class)->make(['status' => Book::STATUS_OPEN]));
+        })->first();
+        $book = $person->books->first();
+
+        $headers = [
+            'Authorization' => 'Bearer ' . $person->api_token
+        ];
+
+        $response = $this->post(route('api.library.add', ['id' => $book->id]), [], $headers);
+
+        $response->assertJson([
+            'success' => false,
+            'code' => 403
+        ]);
+
+        $this->assertDatabaseMissing('users_library', [
+            'user_id' => $person->id,
+            'book_id' => $book->id,
+        ]);
     }
 }
