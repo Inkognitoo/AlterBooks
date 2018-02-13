ee9e230d9d6c33ce6f3fc3ef346acf4921f89570
Pavel Sotnikov <inkognitoo92@yandex.ru>
2018-02-14 00:19:05 +0500

    Добавлена 404-ая страница

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,6 +17,7 @@ mix.js(['resources/assets/js/app.js',
     .sass('resources/assets/sass/app.scss', 'public/css')
     .less('resources/less/style.less', 'public/css')
     .styles('resources/css/fix.css', 'public/css/fix.css')
+    .styles('resources/css/404.css', 'public/css/404.css')
     .sourceMaps()
     .version()
 ;
