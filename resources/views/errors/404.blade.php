8e7b6971a10c99cdbaedc12e2e4ad39e2a65c152
Silvamorta <sagtanas94@mail.ru>
2018-02-23 19:31:42 +0500

    Добавлено оформление сортировки по жанрам
    +  оформление жанров в библиотеке
    +- на всех шаблонах исправлены иконки (все пути. в т.ч. манифест)

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -8,11 +8,11 @@
     <meta name="viewport" content="width=device-width">
 
     <!--Icons-->
-    <link rel="manifest" href="/manifest.json">
-    <link rel="icon" type="image/png" href={{url("/img/icon-16.png")}} sizes="16x16">
-    <link rel="icon" type="image/png" href={{url("/img/icon-32.png")}} sizes="32x32">
-    <link rel="icon" type="image/png" href={{url("/img/icon-180.png")}} sizes="180x180">
-    <link rel="icon" type="image/png" href={{url("/img/icon-192.png")}} sizes="192x192">
+    <link rel="manifest" href={{url('/manifest.json')}}>
+    <link rel="icon" type="image/png" href={{url('/img/icon-16.png')}} sizes="16x16">
+    <link rel="icon" type="image/png" href={{url('/img/icon-32.png')}} sizes="32x32">
+    <link rel="icon" type="image/png" href={{url('/img/icon-180.png')}} sizes="180x180">
+    <link rel="icon" type="image/png" href={{url('/img/icon-192.png')}} sizes="192x192">
 </head>
 <body>
 <main class="container">
