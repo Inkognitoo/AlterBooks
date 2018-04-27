a6343a9d33e6162d76cb1c26e337166f52c37ac9
alex <zanec92@vagrant.pc>
2018-04-27 13:09:29 +0300

    Тесты для онлайн статусов

--- tests/Unit/OnlineStatus/CheckOnlineStatusTest.php
+++ tests/Unit/OnlineStatus/CheckOnlineStatusTest.php
@@ -17,15 +17,50 @@ class CheckOnlineStatusTest extends TestCase
     use DatabaseTransactions;
 
     /**
-     * Проверяем работу оффлайн статуса
+     * Проверяем возврат оффлайн статуса, если last_activity_at пользователя больше, чем ONLINE_ENDED
      *
      * @return void
      */
-    public function testOfflineStatus()
+    public function testLastActivityIsMoreThenOnlineEnded()
     {
         $person = factory(User::class)->create();
         $person->last_activity_at = Carbon::parse($person->last_activity_at)
             ->subMinutes($person::ONLINE_ENDED + 1);
         $this->assertFalse($person->isOnline());
     }
+
+    /**
+     * Проверяем возврат онлайн статуса, если last_activity_at пользователя равно текущему времени
+     *
+     * @return void
+     */
+    public function testLastActivityIsTheCurrentTime()
+    {
+        $person = factory(User::class)->create();
+        $person->last_activity_at = Carbon::now();
+        $this->assertTrue($person->isOnline());
+    }
+
+    /**
+     * Проверяем возврат онлайн статуса, если last_activity_at пользователя меньше, чем ONLINE_ENDED
+     *
+     * @return void
+     */
+    public function testLastActivityIsLessThenOnlineEnded()
+    {
+        $person = factory(User::class)->create();
+        $person->last_activity_at = Carbon::parse($person->last_activity_at)
+            ->subMinutes($person::ONLINE_ENDED - 1);
+        $this->assertTrue($person->isOnline());
+    }
+
+    /**
+     * Проверка изменения last_activity_at в большую сторону при посещении страниц
+     *
+     * @return void
+     */
+    /*public function testLastActivityAtChangeWhenVisitingPages()
+    {
+
+    }*/
 }
