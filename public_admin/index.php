07aca4d77126da3271e8dfef184a752a0779e4dc
Pavel Sotnikov <inkognitoo92@yandex.ru>
2018-06-04 23:17:01 +0500

    Добавлена отдельная точка входа для админки

--- public_admin/index.php
+++ public_admin/index.php
@@ -8,7 +8,7 @@
  */
 
 define('LARAVEL_START', microtime(true));
-define('IS_ADMIN_ENVIRONMENT', false);
+define('IS_ADMIN_ENVIRONMENT', true);
 
 /*
 |--------------------------------------------------------------------------
