130b9f3e38505745532afcd2cc9aa788bd55a756
Pavel Sotnikov <inkognitoo92@yandex.ru>
2018-06-08 20:43:24 +0500

    Отмечать активную категорию

--- resources/views/admin/layouts/left-menu.blade.php
+++ resources/views/admin/layouts/left-menu.blade.php
@@ -1,7 +1,7 @@
 <div id="m_ver_menu" class="m-aside-menu  m-aside-menu--skin-dark m-aside-menu--submenu-skin-dark"
      data-menu-vertical="true" data-menu-scrollable="false" data-menu-dropdown-timeout="500">
     <ul class="m-menu__nav  m-menu__nav--dropdown-submenu-arrow ">
-        <li class="m-menu__item  m-menu__item--active" aria-haspopup="true" >
+        <li class="m-menu__item  m-menu__item{{ Request::is('dashboard') ? '--active' : '' }}" aria-haspopup="true" >
             <a href="{{ route('dashboard') }}" class="m-menu__link ">
                 <i class="m-menu__link-icon flaticon-line-graph"></i>
                 <span class="m-menu__link-title">
