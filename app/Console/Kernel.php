31df062ea3eb82d3f43d0375dfb880908057a582
Pavel Sotnikov <inkognitoo92@yandex.ru>
2018-06-08 21:11:28 +0500

    Исправляем ошибку с несуществующей константой в консольных приложениях

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,8 @@
 namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
@@ -15,6 +17,22 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         //
     ];
+
+    /**
+     * Create a new console kernel instance.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     * @param  \Illuminate\Contracts\Events\Dispatcher  $events
+     * @return void
+     */
+    public function __construct(Application $app, Dispatcher $events)
+    {
+        if (! \defined('IS_ADMIN_ENVIRONMENT')) {
+            \define('IS_ADMIN_ENVIRONMENT', false);
+        }
+
+        parent::__construct($app, $events);
+    }
 
     /**
      * Define the application's command schedule.
