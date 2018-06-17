09382d6cd99daf537c2eb1f6ea4bc908c6ea7cca
Pavel Sotnikov <inkognitoo92@yandex.ru>
2018-06-17 21:22:19 +0500

    Кнопки создания новой сущности

--- resources/views/admin/genre/list.blade.php
+++ resources/views/admin/genre/list.blade.php
@@ -23,6 +23,20 @@
                 </h3>
             </div>
         </div>
+        <div class="m-portlet__head-tools">
+            <ul class="m-portlet__nav">
+                <li class="m-portlet__nav-item">
+                    <a href="#" class="btn btn-primary m-btn m-btn--custom m-btn--pill m-btn--icon m-btn--air">
+                            <span>
+                                <i class="la la-plus"></i>
+                                <span>
+                                    Создать
+                                </span>
+                            </span>
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
     <div class="m-portlet__body">
         <!--begin: Datatable -->
