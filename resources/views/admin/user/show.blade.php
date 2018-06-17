05aa550e2d8f8c5d01e2dab7d0c82d6e6e2a71f6
Pavel Sotnikov <inkognitoo92@yandex.ru>
2018-06-17 22:04:39 +0500

    Кнопки удаления для пользователя

--- resources/views/admin/user/show.blade.php
+++ resources/views/admin/user/show.blade.php
@@ -70,7 +70,50 @@ $attributes = [
                 </div>
             </div>
             <!--end::Section-->
+
+            <a href="#" class="btn btn-primary m-btn m-btn--custom m-btn--pill m-btn--icon m-btn--air">
+                <span>
+                    <i class="la la-close"></i>
+                    <span>
+                        Мягко удалить
+                    </span>
+                </span>
+            </a>
+
+            <button type="button" class="btn btn-danger m-btn m-btn--custom m-btn--pill m-btn--icon m-btn--air" data-toggle="modal" data-target="#delete_modal">
+                <span>
+                    <i class="la la-warning"></i>
+                    <span>
+                        Жёстко удалить
+                    </span>
+                </span>
+            </button>
         </div>
-        <!--end::Form-->
+    </div>
+
+    <div class="modal fade" id="delete_modal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" style="display: none;" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="deleteModalLabel">
+                        Вы действительно хотите удалить пользователя "{{ $user->full_name }}"?
+                    </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>
+                        Внимание! <br>
+                        Это действие необратимо и пользователь будет навсегда удалён из базы данных. <br>
+                        Если вы не уверены в своих действиях, то лучше используйте мягкое удаление.
+                    </p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">Отмена</button>
+                    <button type="button" class="btn btn-danger">Удалить</button>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
