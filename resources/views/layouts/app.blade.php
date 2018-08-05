62269e782bdfa8b1c105623de96fcd25957e44db
Silvamorta <sagtanas94@mail.ru>
2018-08-05 15:06:04 +0500

    Добавлено: футер

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -123,6 +123,85 @@
     </div>
 </main>
 
+<footer class="footer">
+    <div class="footer__area">
+        <div class="footer-info footer-block">
+            <div class="footer-block__title">
+                информация
+            </div>
+            <div class="footer-block__element">
+                О проекте
+            </div>
+            <div class="footer-block__element">
+                Пользовательское&nbsp;соглашение
+            </div>
+            <div class="footer-block__element">
+                FAQ
+            </div>
+            <div class="footer-block__element">
+                Блог
+            </div>
+            <div class="footer-copyright footer-copyright_max">
+                <div class="footer-copyright__text">
+                    © 2014-2018 AlterBooks
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-social footer-block">
+            <div class="footer-block__title">
+                мы&nbsp;в&nbsp;соцсетях
+            </div>
+            <div class="footer-social__area">
+                <a class="footer-social__element footer-social__element_vk"
+                   href="https://vk.com/alterbooks"
+                   title="Вконтакте"></a>
+                <a class="footer-social__element footer-social__element_disabled footer-social__element_facebook"
+                   title="в разработке"></a>
+                <a class="footer-social__element footer-social__element_disabled footer-social__element_twitter"
+                   title="в разработке"></a>
+            </div>
+            <div class="footer-warning">
+                <div class="footer-warning__icon">18+</div>
+                <div class="footer-warning__text">
+                    Внимание! Сайт может содержать информацию, не&nbsp;предназначенную
+                    для&nbsp;просмотра лицами, не&nbsp;достигшими 18&nbsp;лет!
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-block footer-block_max">
+            <div class="footer-copyright footer-copyright_max">
+                <div class="footer-copyright__logo"></div>
+                <div class="footer-copyright__text">
+                    © 2014-2018 AlterBooks
+                </div>
+            </div>
+            <div class="footer-warning">
+                <div class="footer-warning__icon">18+</div>
+                <div class="footer-warning__text">
+                    Внимание! Сайт может содержать информацию, не&nbsp;предназначенную
+                    для&nbsp;просмотра лицами, не&nbsp;достигшими 18&nbsp;лет!
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer__area footer__area_bottom">
+        <div class="footer-warning">
+            <div class="footer-warning__icon">18+</div>
+            <div class="footer-warning__text">
+                Внимание! Сайт может содержать материалы, не&nbsp;предназначенные
+                для&nbsp;просмотра лицами, не&nbsp;достигшими&nbsp;18&nbsp;лет!
+            </div>
+        </div>
+        <div class="footer-copyright footer-copyright_max">
+            <div class="footer-copyright__text">
+                © 2014-{{ date('Y') }} AlterBooks
+            </div>
+        </div>
+    </div>
+</footer>
+
 @include("auth.login")
 
 <script src="{{ mix('/js/app.js') }}"></script>
