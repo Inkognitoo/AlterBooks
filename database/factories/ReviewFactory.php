4386e4ec0428012040d7179a1400c40e6dcb777e
Silvamorta <sagtanas94@mail.ru>
2018-08-09 13:50:54 +0500

    Добавлено: заголовк рецензии в генерации фейковых данных

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -7,6 +7,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Models\Review::class, function (Faker $faker) {
     return [
         'rating' => rand(1, 10),
+        'header' => mb_convert_encoding($faker->realText(rand(20, 67)), 'UTF-8'),
         'text' => mb_convert_encoding($faker->realText(rand(100, 500)), 'UTF-8'),
     ];
 });
