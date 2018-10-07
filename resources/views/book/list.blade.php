432899841768adcef154c5a5c8471c8ccfffc9e1
Pavel Sotnikov <inkognitoo92@yandex.ru>
2018-10-07 11:19:01 +0500

    Исправлена проблема с неработающим центрированием

--- resources/views/book/list.blade.php
+++ resources/views/book/list.blade.php
@@ -3,32 +3,34 @@
 @section('title', 'Список книг')
 
 @section('content')
-<div id="app" style="display: flex">
-    <div class="book-list-aside col-3 col-clear col-lg-0">
-        <genres-panel v-on:change-active-genres="changeActiveGenres" v-bind:all-genres="books.genres" v-bind:active-genres="books.filtered.genres"></genres-panel>
-    </div>
-
-    <div class="book-list-main col-8 col-clear col-lg-10 col-md-11 col-sm-12">
-        <div class="row">
-            <div class="col-12 col-clear">
-                <search-form></search-form>
+    <div class="col-12 col-center">
+        <div id="app" class="row row-center">
+            <div class="book-list-aside col-3 col-clear col-lg-0">
+                <genres-panel v-on:change-active-genres="changeActiveGenres" v-bind:all-genres="books.genres" v-bind:active-genres="books.filtered.genres"></genres-panel>
             </div>
 
-            <div class="book-list-sort col-12 col-clear">
-                <sort-panel v-on:change-active-sort="changeActiveSort" v-bind:sort="books.sorted.sort"></sort-panel>
-            </div>
+            <div class="book-list-main col-8 col-clear col-lg-10 col-md-11 col-sm-12">
+                <div class="row">
+                    <div class="col-12 col-clear">
+                        <search-form></search-form>
+                    </div>
 
-            <div class="col-0 col-lg-12 col-lg-clear">
-                <genres-panel v-on:change-active-genres="changeActiveGenres" v-bind:all-genres="books.genres" v-bind:active-genres="books.filtered.genres" v-bind:mini="true"></genres-panel>
-            </div>
+                    <div class="book-list-sort col-12 col-clear">
+                        <sort-panel v-on:change-active-sort="changeActiveSort" v-bind:sort="books.sorted.sort"></sort-panel>
+                    </div>
 
-            <book v-bind:book="book" v-bind:active-genres="books.filtered.genres" v-bind:key="book.id" v-for="book in books.data"></book>
-            <div v-if="books.total === 0" class="text-center">
-                Нет ни одной книги, доступной для чтения
-            </div>
+                    <div class="col-0 col-lg-12 col-lg-clear">
+                        <genres-panel v-on:change-active-genres="changeActiveGenres" v-bind:all-genres="books.genres" v-bind:active-genres="books.filtered.genres" v-bind:mini="true"></genres-panel>
+                    </div>
 
-            <paginate v-on:change-active-page="changeActivePage" v-bind:page-count="books.pageCount" v-bind:current-page="books.currentPage"></paginate>
+                    <book v-bind:book="book" v-bind:active-genres="books.filtered.genres" v-bind:key="book.id" v-for="book in books.data"></book>
+                    <div v-if="books.total === 0" class="text-center">
+                        Нет ни одной книги, доступной для чтения
+                    </div>
+
+                    <paginate v-on:change-active-page="changeActivePage" v-bind:page-count="books.pageCount" v-bind:current-page="books.currentPage"></paginate>
+                </div>
+            </div>
         </div>
     </div>
-</div>
 @endsection
