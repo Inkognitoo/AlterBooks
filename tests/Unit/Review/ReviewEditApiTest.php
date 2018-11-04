3b6c3c960a4501e9c343d82d0d3b6e4b12bcd806
Silvamorta <sargatanas94@mail.ru>
2018-11-04 11:15:47 +0500

    Промежуточное: тест на успешное редактированеи рецензии #1

---
+++ tests/Unit/Review/ReviewEditApiTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit\Registration;
+
+use App\Models\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ReviewEditApiTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    use DatabaseTransactions;
+
+
+}
