35e5f22f507af9ceeaf605187a3e58fa469cdd3a
Ваше Имя <you@example.com>
2018-11-08 21:08:26 +0300

    Добавлены отношения между пользователем и статьей блога

--- app/Models/Blog/Article.php
+++ app/Models/Blog/Article.php
@@ -3,19 +3,36 @@
 namespace App\Models\Blog;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\User;
 
 
 class Article extends Model
 {
     use SoftDeletes, Sluggable;
 
-    protected $dates = ['deleted_at'];
-
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'title',
         'text'
     ];
 
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
     public function sluggable()
     {
         return [
@@ -23,5 +40,15 @@ class Article extends Model
                 'source' => 'title'
             ]
         ];
+    }
+
+    /**
+     * Автор статьи
+     *
+     * @return BelongsTo
+     */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
