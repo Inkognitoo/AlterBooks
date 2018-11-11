285c2abfd921f05e14eed6c125145bf7085e0d45
Silvamorta <sargatanas94@mail.ru>
2018-11-11 15:11:19 +0500

    Добавлено: тест на успешное удаление рецензии #1

--- tests/Unit/Review/ReviewDeleteApiTest.php
+++ tests/Unit/Review/ReviewDeleteApiTest.php
@@ -2,30 +2,22 @@
 
 namespace Tests\Unit\Review;
 
+use App\Models\Book;
+use App\Models\Review;
 use App\Models\User;
-use Illuminate\Foundation\Testing\WithFaker;
+use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use ReviewEditTestSeeder;
 
-class ReviewEditApiTest extends TestCase
+class ReviewDeleteApiTest extends TestCase
 {
     use DatabaseMigrations;
 
     use DatabaseTransactions;
 
-    use WithFaker;
-
-    public function setUp()
-    {
-        parent::setUp();
-
-        $this->seed(ReviewEditTestSeeder::class);
-    }
-
     /**
-     * Успешное редактирование рецензии
+     * Успешное удаление рецензии
      *
      * @return void
      * @throws \Exception
@@ -33,35 +25,30 @@ class ReviewEditApiTest extends TestCase
     public function testReviewEditSuccess()
     {
         /** @var User $user */
-        $user = User::inRandomOrder()->first();
-        $review = $user->reviews()->inRandomOrder()->first();
+        $user = factory(User::class)->create();
+
+        /** @var User $alter_user */
+        $alter_user = factory(User::class)->create();
+
+        /** @var Book $book*/
+        $book = $alter_user->books()->save(factory(Book::class)->make(['status' => Book::STATUS_OPEN]));
+
+        /** @var Review $review*/
+        $review = $user->reviews()->save(factory(Review::class)->make([
+            'book_id' => $book->id
+        ]));
 
         $headers = [
             'Authorization' => 'Bearer ' . $user->api_token
         ];
 
-        do {
-            $review_rating = random_int(1, 10);
-        } while ($review_rating === $review->rating);
-
-        do {
-            $review_header = mb_convert_encoding($this->faker->realText(random_int(20, 67)), 'UTF-8');
-        } while ($review_header === $review->header);
-
-        do {
-            $review_text = mb_convert_encoding($this->faker->realText(random_int(100, 500)), 'UTF-8');
-        } while ($review_text === $review->text);
-
-
-        $response = $this->put(route('api.review.edit', ['book_id' => $review->book_id, 'id' => $review->id]), ['rating' => $review_rating, 'header' => $review_header, 'text' => $review_text], $headers);
+        $response = $this->delete(route('api.review.delete', ['book_id' => $review->book_id, 'id' => $review->id]), [], $headers);
         $response->assertJson([
             'success' => true
         ]);
         $this->assertDatabaseHas('reviews', [
             'id' => $review->id,
-            'rating' => $review_rating,
-            'header' => $review_header,
-            'text' => $review_text
+            'deleted_at' => Carbon::now()
         ]);
     }
 }
