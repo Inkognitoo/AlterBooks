4eda5f1fbfa441fae6cd2833ee02819897e6b724
alex <zanec92@vagrant.pc>
2018-12-04 16:32:32 +0300

    Страница создания статьи блога

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ArticleCreateRequest;
+use App\Http\Middleware\CheckAuth;
+use App\Models\Blog\Article;
+use Auth;
+
+class ArticleController extends Controller
+{
+    /**
+     * Проверка авторизации пользователя
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware(CheckAuth::class);
+    }
+
+    /**
+     * Показываем страницу создания статьи блога
+     *
+     * @return Response
+     */
+    public function createShow()
+    {
+        return view('blog.create');
+    }
+
+    /**
+     * Создаем статью блога
+     * @param ArticleCreateRequest $request
+     * @return Response
+     * @throws Exception
+     */
+    public function create(ArticleCreateRequest $request)
+    {
+        $article = new Article($request->all());
+        Auth::user()->articles()->save($article);
+
+        return redirect()->back();
+    }
+}
---
+++ resources/views/blog/create.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@section('title', 'Создать статьи')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        {{ t('article', 'Создание статьи') }}
+                    </div>
+
+                    <div class="panel-body">
+
+                        <form class="form-horizontal" method="POST" action="{{ route('blog.create') }}">
+                            {{ csrf_field() }}
+                            <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+                                <label for="title" class="col-md-4 control-label">
+                                    {{ t('article', 'Название') }}
+                                </label>
+
+                                <div class="col-md-6">
+                                    <input id="title" type="text" class="form-control" name="title"
+                                           value="{{ old('title') }}" autofocus >
+
+                                    @if ($errors->has('title'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('title') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="form-group{{ $errors->has('text') ? ' has-error' : '' }}">
+                                <label for="text" class="col-md-4 control-label">
+                                    {{ t('article', 'Текст') }}
+                                </label>
+
+                                <div class="col-md-6">
+                                <textarea id="description" class="form-control" name="text"
+                                          autofocus rows="5">
+                                </textarea>
+
+                                    @if ($errors->has('text'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('text') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ t('blog.button', 'Создать') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
