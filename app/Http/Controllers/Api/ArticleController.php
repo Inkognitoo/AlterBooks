412848f2b8914010c9e2699329251b89daa3abd2
Alex <you@example.com>
2018-12-15 16:21:36 +0300

    API для создания статьи блога

---
+++ app/Http/Controllers/Api/ArticleController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Middleware\Api\ApiWrapper;
+use App\Http\Middleware\CheckAuth;
+use App\Http\Requests\Api\ArticleCreateRequest;
+
+class ArticleController extends Controller
+{
+    /**
+     * Instantiate a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware(CheckAuth::class);
+        $this->middleware(ApiWrapper::class);
+    }
+
+    /**
+     * Создаем статью блога
+     *
+     * @param ArticleCreateRequest $request
+     * @throws \Exception
+     */
+    public function create(ArticleCreateRequest $request)
+    {
+        $article = new Article($request->all());
+        Auth::user()->articles()->save($article);
+    }
+}
