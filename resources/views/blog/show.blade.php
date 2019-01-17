f7100af7b6e07c2161e40017c871c5930d22b913
Alex <you@example.com>
2019-01-17 20:58:46 +0300

    Отображение даты в статье блога, и если автор создатель статьи, то отобразить ссылку "Редактировать статью"

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -16,9 +16,15 @@
                         {!! $article->text !!}
                     </div>
                 </div>
-                <div class="col-9">
-                    <span><i class="glyphicon glyphicon-user"></i>{{ $article->author->full_name }}</span>
+                <div class="col-9" style="margin-top: 20px">
+                    <span style="padding-right: 100px">{{ $article->author->full_name }}</span>
+                    <span>{{ $article->created_at->format('d-m-Y') }}</span>
                 </div>
+                @if(Auth::user()->id == $article->author->id)
+                    <div class="col-9" style="margin-top: 20px">
+                        <a href="#">Редактировать статью</a>
+                    </div>
+                @endif
             </div>
         </div>
     </div>
